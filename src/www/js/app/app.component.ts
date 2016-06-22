dbaed38daa2ca90a09e61b77b42c09687abc333c
Eric W. Greene <eric@ericwgreene.com>
2016-06-22 17:01:01 -0500

    added observable

--- src/www/js/app/app.component.ts
+++ src/www/js/app/app.component.ts
@@ -1,53 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+
+import { Observable, Subscription } from 'rxjs/Rx';
+
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: 'a' })
-export class APipe implements PipeTransform {
-	transform(value: string) {
-		console.log('pipe a ran');
-		return value;
-	}
-}
-
-@Pipe({ name: 'b', pure: false })
-export class BPipe implements PipeTransform {
-	transform(value: string) {
-		console.log('pipe b ran');
-		return value;
-	}
-}
-
-@Pipe({ name: 'sort', pure: false })
-export class SortPipe implements PipeTransform {
-	transform(values: string[]) {
-		console.log('sort');
-		return values.sort();
-
-	}
-}
+// @Pipe({ name: 'myAsync', pure: false })
+// export class MyAsyncPipe implements PipeTransform {
+// 	transform(value: any) {
+// 		value.subscribe(function(v) {
+// 			return v;
+// 		})
+// 	}
+// }
 
 
 @Component({
 	selector: 'my-app',
-	template: `
-		<input type="text" [(ngModel)]="aText">{{aText | a}}<br>
-		<input type="text" [(ngModel)]="bText">{{bText | b}}<br>
-		<ul>
-			<li *ngFor="let color of colors | sort">{{color}}</li>
-		</ul>
-		New Color: <input [(ngModel)]="newColor" type="text">
-		<button (click)="addColor()">Add Color</button>
-	`,
-	pipes: [ APipe, BPipe, SortPipe ]
+	template: `<h1>{{message | async}}</h1>`
 })
-export class AppComponent {
-	aText: string;
-	bText: string;
-	colors: string[] = ['red','white','blue'];
-	newColor: string;
+export class AppComponent implements OnInit {
 
-	addColor() {
-		this.colors.push(this.newColor);
-		//this.colors = this.colors.concat(this.newColor);
+	message: Observable<number>;
+
+	ngOnInit() {
+
+		//var o: Observable<number> = Observable.range(1,5);
+
+		var o: Observable<number> = Observable.create(function(observer) {
+
+			var t=0;
+
+			setInterval(function() {
+				observer.next(t++);
+			},500);
+
+		});
+
+		this.message = o.map(num => num * 2);
+
+
 	}
+
 }
