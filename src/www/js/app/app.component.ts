80beaa240253eb23bbf5939771533843c9cf1601
Eric W. Greene <eric@ericwgreene.com>
2016-06-22 13:58:00 -0500

    pushing up pipes

--- src/www/js/app/app.component.ts
+++ src/www/js/app/app.component.ts
@@ -1,16 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import { Pipe, PipeTransform } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  template: `<h1>Car Manager</h1>
-	<router-outlet></router-outlet>`,
-	directives: [ ROUTER_DIRECTIVES ]
-})
-export class AppComponent implements OnInit {
+@Pipe({ name: 'a' })
+export class APipe implements PipeTransform {
+	transform(value: string) {
+		console.log('pipe a ran');
+		return value;
+	}
+}
 
-	ngOnInit() {
+@Pipe({ name: 'b', pure: false })
+export class BPipe implements PipeTransform {
+	transform(value: string) {
+		console.log('pipe b ran');
+		return value;
+	}
+}
+
+@Pipe({ name: 'sort', pure: false })
+export class SortPipe implements PipeTransform {
+	transform(values: string[]) {
+		console.log('sort');
+		return values.sort();
 
 	}
+}
 
+
+@Component({
+	selector: 'my-app',
+	template: `
+		<input type="text" [(ngModel)]="aText">{{aText | a}}<br>
+		<input type="text" [(ngModel)]="bText">{{bText | b}}<br>
+		<ul>
+			<li *ngFor="let color of colors | sort">{{color}}</li>
+		</ul>
+		New Color: <input [(ngModel)]="newColor" type="text">
+		<button (click)="addColor()">Add Color</button>
+	`,
+	pipes: [ APipe, BPipe, SortPipe ]
+})
+export class AppComponent {
+	aText: string;
+	bText: string;
+	colors: string[] = ['red','white','blue'];
+	newColor: string;
+
+	addColor() {
+		this.colors.push(this.newColor);
+		//this.colors = this.colors.concat(this.newColor);
+	}
 }
--- src/www/js/app/main.ts
+++ src/www/js/app/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component.ts';
-import { Cars } from './services/cars.ts';
-import { APP_ROUTER_PROVIDER } from './routes.ts';
+//import { Cars } from './services/cars.ts';
+//import { APP_ROUTER_PROVIDER } from './routes.ts';
 
-bootstrap(AppComponent, [ APP_ROUTER_PROVIDER, Cars ]);
+//bootstrap(AppComponent, [ APP_ROUTER_PROVIDER, Cars ]);
+bootstrap(AppComponent);
